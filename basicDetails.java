926baa9f2daba3fc68558b570b9c8ae7fcae775d
saktheswarar <130922071+saktheswarar@users.noreply.github.com>
2023-07-18 18:12:08 +0530

    Add files via upload

---
+++ basicDetails.java
@@ -0,0 +1,38 @@
+package sakthe2;
+
+public class basicDetails {
+	public static void main (String args[]) {
+		//data type variable name=value;
+		byte a=89;
+		byte a1=27;
+		byte a2=8;
+		short b=2002;
+		long c=6379087060L;
+		float d=87.9F;
+		char e=':';
+		char e1='-';
+		char e2 ='%';
+		char e3='/';
+		char e4='p';
+		char e5='.';
+		char e6=',';
+		String f="sakthe";
+		String f1="language known";
+		String f2="phone number";
+		String f3="date of brith";
+		String f4="mark";
+		String f5="percentage";
+		String f6="name";
+		String f7="tamil";
+		String f8="english";
+		System.out.println(f6+e+e4+e5+f);
+		System.out.println(f3+e+a1+e3+a2+e3+b);
+		System.out.println(f1+e+f7+e6+f8);
+		System.out.println(f4+e+a);
+		System.out.println(f5+e+d+e2);
+		System.out.println(f2+e+c);
+		
+
+	}
+
+}
