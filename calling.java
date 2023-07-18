87244021c93ae59e53b08aa01e658dbd91ef1c1d
saktheswarar <130922071+saktheswarar@users.noreply.github.com>
2023-07-18 18:14:36 +0530

    Add files via upload

---
+++ calling.java
@@ -0,0 +1,18 @@
+package sakthe2;
+
+public class calling {
+	public static void main(String args[]) {
+		anto z= new anto();
+		anto x= new anto();
+		anto y= new anto();
+		
+		z.setc(22);
+		System.out.println(z.getc());
+		x.setg("anto");
+		
+		y.seth(23.4f);
+		System.out.println(x.getg());
+		System.out.println(y.geth());
+	}
+
+}
