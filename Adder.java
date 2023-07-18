b21d6b7ec9f4c5264543800b5c84e2fe9b84ad72
saktheswarar <130922071+saktheswarar@users.noreply.github.com>
2023-07-18 18:10:29 +0530

    Add files via upload

---
+++ Adder.java
@@ -0,0 +1,18 @@
+package sakthe2;
+
+public class Adder {
+	static int value(int i) {
+		return i;
+	}
+	static int value(int mark,int result) {
+		return result+mark;
+		
+ 	}
+	public static void main(String args[]) {
+		System.out.println(value(2));
+		System.out.println(value(22,77));
+		
+		
+	}
+
+}
