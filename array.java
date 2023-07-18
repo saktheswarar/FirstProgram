ae9b788265777e35906c11dea934d3223b94b4ff
saktheswarar <130922071+saktheswarar@users.noreply.github.com>
2023-07-18 18:11:27 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,20 @@
+package sakthe2;
+
+public class array {
+	public static void main(String args[]) {
+		
+		//initialization
+		/*int arr[]=new int[6];//012345
+		arr[0]=365;
+		arr[2]=35;
+		arr[4]=3;*/
+		
+		//instantiation
+		int arr[]= {1,2,3,4,5,6};
+		for(int a=0; a<=5; a++) {
+			System.out.print(arr[a]+"  ");
+			
+		}
+	}
+
+}
